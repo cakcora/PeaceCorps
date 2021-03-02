18ccdba7b8d3d5f4a4e1011ea71b183ea7ea1261
Cuneyt Akcora <cuneytgurcan@yahoo.com>
2021-03-02 15:37:47 -0600

    de

--- CoralTda/1CoralExperiments.R
+++ CoralTda/1CoralExperiments.R
@@ -7,13 +7,23 @@ options(java.parameters = "-Xmx200g")
 
 #  Graph ML datasets
 dataPath <- "C:/data/"
+dataPath <- "/home/jupiter/GraphML/"
+feature = "degree"
+# max dimension of the homological features to be computed. (e.g. 0 for connected components, 1 for connected components and loops, 2 for connected components, loops, voids, etc.)
+#maxDimension <- 2
 
-loadGraph<-function(dataset, dataAlias){
-  whichOutputFile<-paste0(outputPath,dataAlias,outputFile)
-  # Remove earlier result files, if they exist
-  if (file.exists(whichOutputFile)) {
-    file.remove(whichOutputFile)
-  }
+# upper limit on the simplex dimension, size of the cliques to find
+maxClique <-5
+
+#betti 2 icin max simple size 3 veya ustunde olmasi bir sey farkettirmeyecek.
+#betti 3 icin 4 ve uzeri olmasi farketmez.
+
+testRange<-c(1,2,3,4)
+
+dataset="NCI1/NCI1."
+dataAlias="NCI1"
+source("nodeFunctions.R")
+loadGraph<-function(dataset="NCI1/NCI1.", dataAlias="NCI1"){
   edgeFile <- paste0(dataPath, dataset, "edges")
   graphFile <- paste0(dataPath, dataset, "graph_idx")
   graphIdData <-
@@ -30,6 +40,7 @@ loadGraph<-function(dataset, dataAlias){
   graphs <- unique(graphIdData$V1)
   message("Processing ",dataAlias," for feature ",feature,". Num of Graphs: ",length(graphs))
   results<-data.frame()
+  times<-c()
   for (graphId in graphs) {
     # locate nodes of the graph
     thisGraphNodes <- which(graphIdData$V1 == graphId)
@@ -39,63 +50,90 @@ loadGraph<-function(dataset, dataAlias){
                 allData$target %in% thisGraphNodes, ]
     graph <- graph.data.frame(edgeData, directed = FALSE)
     nodeCount <- vcount(graph)
-     
-      # below we use sub level filtrations.
-      
-      #sublevel filtration
-      # compute node function values 
-      tryCatch({
-        F.values = computeNodeVals(feature, graph,dataset=dataset,dataPath=dataPath)
-      }, error = function(error_condition) {
-        message("Error in extracting PDs for ",dataAlias)
-        return(-1);
-      })
-      # for maxClique=3 below means we are adding 0,1,2 simplices (nodes,edges,triangles) to our complex
-      cmplx <- cliques(as.undirected(graph), min = 0, max = maxClique)
-      # use sublevel=T for sublevel, sublevel=F for superlevel filtration
-      # F.values are node values. At these values, node appear in the complex,
-      # and their edges to other active nodes also activate.
-      FltRips <- funFiltration(FUNvalues = F.values,
-                               cmplx = cmplx,
-                               sublevel = T) # Construct filtration using F.values
-      
-      #extract the persistence diagram
-      persistenceDiagram <-
-        filtrationDiag(filtration = FltRips, maxdimension = maxDimension)$diagram
-      
-      #extract betti number - unused
-      # b0OrigGraph <- sum(persistenceDiagram[, 1] == 0)
-      # b1OrigGraph <- sum(persistenceDiagram[, 1] == 1)
-      # b2OrigGraph <- sum(persistenceDiagram[, 1] == 2)
-      
-      #extract birth and death times
-      pd2 <-cbind(persistenceDiagram,GraphId = graphId,dataset = dataAlias)
-      results<-rbind(results,pd2)
-      
     
+    # below we use sub level filtrations.
+    nGraph<-as.undirected(graph)
+    timer<-system.time({
+      for(maxDimension in testRange){
+        
+        #sublevel filtration
+        # compute node function values 
+        tryCatch({
+          F.values = degree(nGraph, v = V(nGraph), mode = c("all"));
+        }, error = function(error_condition) {
+          message("Error in extracting PDs for ",dataAlias)
+          return(-1);
+        })
+        # for maxClique=3 below means we are adding 0,1,2 simplices (nodes,edges,triangles) to our complex
+       
+        cmplx <- cliques(nGraph, min = 0, max = maxClique)
+        # use sublevel=T for sublevel, sublevel=F for superlevel filtration
+        # F.values are node values. At these values, node appear in the complex,
+        # and their edges to other active nodes also activate.
+        FltRips <- funFiltration(FUNvalues = F.values,
+                                 cmplx = cmplx,
+                                 sublevel = T) # Construct filtration using F.values
+        
+        #extract the persistence diagram
+        persistenceDiagram <-
+          filtrationDiag(filtration = FltRips, library="Dionysus", maxdimension = maxDimension)$diagram
+        b0OrigGraph <- sum(persistenceDiagram[, 1] == maxDimension)
+        
+        #extract birth and death times
+        pd0 <-data.frame(count=b0OrigGraph,betti=maxDimension,GraphId = graphId,dataset = dataAlias,style="all")
+        results<-rbind(results,pd0)
+      }
+    })
+    
+    timeAll<-(timer[[1]]);
+    ### coral computations
+    
+    cGraph<-nGraph
+     timer<-system.time({
+    coreness <- graph.coreness(cGraph)
+    for(maxDimension in testRange){
+        cGraph <- induced.subgraph(graph=cGraph,vids=names(which(coreness>maxDimension)))
+        #message(maxDimension, " ",vcount(cGraph))
+        if(vcount(cGraph)>0){
+          tryCatch({
+            #F.values =  apply(as_adjacency_matrix(cGraph), 1, sum)
+            F.values = degree(cGraph, v = V(cGraph), mode = c("all"))
+          }, error = function(error_condition) {
+            message("Error in extracting PDs for ",dataAlias)
+            message(error_condition)
+            return(-1);
+          })
+          
+          cmplx <- cliques(cGraph, min = 0, max = maxClique)
+          # use sublevel=T for sublevel, sublevel=F for superlevel filtration
+          # F.values are node values. At these values, node appear in the complex,
+          # and their edges to other active nodes also activate.
+          FltRips <- funFiltration(FUNvalues = F.values,
+                                   cmplx = cmplx,
+                                   sublevel = T) # Construct filtration using F.values
+          #extract the persistence diagram
+          persistenceDiagram <-
+            filtrationDiag(filtration = FltRips, library="Dionysus",maxdimension = maxDimension)$diagram
+          bCore <- sum(persistenceDiagram[, 1] == maxDimension)
+          #if(maxDimension>1 & bCore>0)
+            #message(maxDimension," ---------------------",bCore)
+          pdCore <-data.frame(count=bCore,betti=maxDimension,GraphId = graphId,dataset = dataAlias,style="core")
+          results<-rbind(results,pdCore)
+        }
+      
+    }
+    })
+    timeCore<-(timer[[1]]);
+    message(graphId," ", timeAll," ", timeCore/timeAll, " ",vcount(cGraph)/vcount(nGraph)," ",
+            ecount(cGraph)/ecount(nGraph))
+    times<-c(times,timeCore/timeAll)
   }
-  write.table(
-    results,
-    file = whichOutputFile,
-    sep = "\t",
-    row.names = FALSE,
-    col.names = FALSE,
-    append = T,
-    quote = FALSE
-  )
+  message(dataAlias," ",mean(times,rm.na=T))
 }
 
 
-loadGraph("NCI1/NCI1.","NCI1",feature=f)
-loadGraph("ENZYMES/ENZYMES.","Enzyme",feature=f)
-loadGraph("BZR/BZR.","BZR",feature=f)
-loadGraph("COX2/COX2.","COX2",feature=f)
-loadGraph("DHFR/DHFR.","DHFR",feature=f)
-loadGraph("FRANKENSTEIN/FRANKENSTEIN.","FRANKENSTEIN",feature=f)
-#extractPD("REDDIT-MULTI-5K/REDDIT-MULTI-5K.","REDDIT5K",feature=f)
-
-loadGraph("proteins/proteins.","Protein",feature=f)
-loadGraph("REDDIT-BINARY/REDDIT-BINARY.","RedditBinary",feature=f)
-loadGraph("IMDB-MULTI/IMDB-MULTI.","IMDBMulti",feature=f)
-
-loadGraph("IMDB-BINARY/IMDB-BINARY.","IMDBBinary",feature=f)
+loadGraph("NCI1/NCI1.","NCI1")
+loadGraph("proteins/proteins.","Protein")
+loadGraph("REDDIT-BINARY/REDDIT-BINARY.","RedditBinary")
+loadGraph("IMDB-MULTI/IMDB-MULTI.","IMDBMulti")
+loadGraph("IMDB-BINARY/IMDB-BINARY.","IMDBBinary")
